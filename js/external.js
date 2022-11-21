a97d0243b5372d1784dcb8aaf9a1ac42821e8ade
Robert Arroyos <robertarroyosjr@gmail.com>
2022-11-21 11:42:44 -0600

    Completed JavaScript with HTML exercise

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log('Hello from external JavaScript');
+
+alert('Welcome to my website!');
+
+color = prompt('What is your favorite color?');
+
+alert('Great! ' + color + ' is my favorit color too.');
